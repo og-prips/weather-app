93319c632f2abebc526a80a6e508aa0e137515e8
Robin Andersson <robin.andersson@me.com>
2023-01-23 17:31:13 +0100

    added loading logo while fetching removed App.test.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import ForecastList from "./components/ForecastList/ForecastList";
 import Search from "./components/Search/Search";
 import CurrentWeather from "./components/CurrentWeather/CurrentWeather";
+import loadingLogo from "./assets/images/loading.svg";
 import "./App.css";
-
-console.log(process.env.REACT_APP_WEATHER_API_KEY);
 
 function App() {
   const [weather, setWeather] = useState(null);
   const [location, setLocation] = useState(null);
   const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -36,12 +36,15 @@ function App() {
 
   const handleSearch = (location) => {
     setError(false);
+    setLoading(true);
     console.log("fetching");
 
     fetch(
       `https://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_WEATHER_API_KEY}&q=${location}&days=6&aqi=no&lang=en`
     )
       .then((response) => {
+        setLoading(false);
+
         if (response.status === 200) {
           return response.json();
         } else {
@@ -58,6 +61,11 @@ function App() {
   return (
     <>
       <Search onSearch={handleSearch} />
+      {loading && (
+        <div className="loading">
+          <img src={loadingLogo} alt="loading" />
+        </div>
+      )}
       {error && <h1 className="warning">{error}</h1>}
       {weather && <CurrentWeather data={weather} />}
       {weather && <ForecastList data={weather} />}
